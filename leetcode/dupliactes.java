21520cd11f5cb2d833b9a72c96c32ddb78788644
linus <dharmenderchugh71@gmail.com>
2024-03-24 04:04:41 +0530

    added more file

--- leetcode/dupliactes.java
+++ leetcode/dupliactes.java
@@ -9,23 +9,35 @@ import java.util.Iterator;
 public class dupliactes {
 public static void main(String[] args) {
     int[] arr = {-3,-1,0,0,0,3,3};
-    Set<Integer> set = new HashSet<>();
-    for (int i= 0;i<arr.length;i++){
-        set.add(arr[i]);
-        System.out.print(arr[i]+" ");
-    }
-    System.out.println(set+"settttttt");
-    Iterator it = set.iterator();
-    int index = 0;
-    while(it.hasNext()){
-        Object element = it.next();
-        arr[index] = Integer.parseInt(element.toString());
-        System.out.println(Arrays.toString(arr));
-        index++;
+    // Set<Integer> set = new HashSet<>();
+    // for (int i= 0;i<arr.length;i++){
+    //     set.add(arr[i]);
+    //     System.out.print(arr[i]+" ");
+    // }
+    // System.out.println(set+"settttttt");
+    // Iterator it = set.iterator();
+    // int index = 0;
+    // while(it.hasNext()){
+    //     Object element = it.next();
+    //     arr[index] = Integer.parseInt(element.toString());
+    //     System.out.println(Arrays.toString(arr));
+    //     index++;
         
+    // }
+    // System.out.println(Arrays.toString(arr));
+    // System.out.println(index);
+    remove_duplicates(arr);
     }
-    System.out.println(Arrays.toString(arr));
-    System.out.println(index);
+    static void remove_duplicates(int[] arr){
+        int counter = 0;
+        for (int j=1;j<arr.length;j++){
+            if (arr[counter] != arr[j]){
+                arr[counter+1] = arr[j];
+                counter ++;
+
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
 
